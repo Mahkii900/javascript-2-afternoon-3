a1109a4ef58e686294b765773027549b30b19f4d
Mahkii900 <col.graff.battleschool@gmail.com>
2019-06-26 15:12:44 -0600

    final commit

--- practice.js
+++ practice.js
@@ -155,7 +155,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
-
+function each(arr, func) {
+  for (var i = 0; i < arr.length; i++) {
+    func(arr[i], i)
+  }
+}
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
@@ -172,7 +176,13 @@ each(names, function(item, indice){
 */
 
 // Code here
-
+function getUserById(arr, idNum, func) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i]['id'] === idNum) {
+      func(arr[i])
+    }
+  }
+}
 // Do not edit the code below.
 var users = [
   {
